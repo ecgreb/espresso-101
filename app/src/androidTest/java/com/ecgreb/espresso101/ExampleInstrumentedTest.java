c1fa377b3db54f463f7325c343b2f59827f03ad6
Chuck Greb <charles.greb@gmail.com>
2017-02-12 10:06:28 -0500

    Initial commit

---
+++ app/src/androidTest/java/com/ecgreb/espresso101/ExampleInstrumentedTest.java
@@ -0,0 +1,41 @@
+package com.ecgreb.espresso101;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.junit.Assert.*;
+
+/**
+ * Instrumentation test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+
+  @Rule
+  public ActivityTestRule<MainActivity> activityRule = new ActivityTestRule(MainActivity.class);
+
+  @Test
+  public void shouldDisplayHelloWorld() {
+    onView(withText("Hello World!")).check(matches(isDisplayed()));
+  }
+
+  @Test
+  public void useAppContext() throws Exception {
+    // Context of the app under test.
+    Context appContext = InstrumentationRegistry.getTargetContext();
+
+    assertEquals("com.ecgreb.espresso101", appContext.getPackageName());
+  }
+}
